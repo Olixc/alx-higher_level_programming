369b6f72234a62f4a65ede6b5f7feea0706cab19
Olixc <noibiabdulrasaq@rocketmail.com>
2023-12-06 19:58:24 +0100

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list == []:
+        return 0
+    else:
+        return sum([i[0] * i[1] for i in my_list]) / \
+               sum([i[1] for i in my_list])
